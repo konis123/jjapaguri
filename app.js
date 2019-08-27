635e35283b8af6c7d5e7df649605c70795f21cde
jjapa <konis123@naver.com>
2019-08-27 20:43:14 +0900

    step6 test

--- app.js
+++ app.js
@@ -2,16 +2,35 @@
 
 var os = require('os');
 var nodeStatic = require('node-static');
-var http = require('http');
-var socketIO = require('socket.io');
+//var socketIO = require('socket.io');
+var express = require('express');
+var app = express();
+var http = require('http').createServer(app);
 
+
+/*
 var fileServer = new(nodeStatic.Server)();
 var app = http.createServer(function(req, res) {
   fileServer.serve(req, res);
 }).listen(8080);
+*/
+
+app.use(express.static('public'))
+
+app.get('/', function(req, res){
+  //res.send('<h1>Hello world</h1>');
+  res.sendFile(__dirname + '/index.html');
+});
+
+http.listen(3000, function(){
+  console.log('listening on *:3000');
+});
+
+///
 
 
-var io = socketIO.listen(app);
+//var io = socketIO.listen(app);
+var io = require('socket.io')(http);
 io.sockets.on('connection', function(socket) {
 
   // convenience function to log server messages on the client
