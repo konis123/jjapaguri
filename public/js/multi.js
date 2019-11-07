37ee21f903240f23832250ffb01753a87e29bbbd
jjapa <konis123@naver.com>
2019-11-07 23:15:05 +0900

    test

--- public/js/multi.js
+++ public/js/multi.js
@@ -17,7 +17,8 @@ var remoteVideosContainer = document.getElementById('remote-videos-container')
 connection.onstream = function(event){
     var video = event.mediaElement;
 
-    if(event.type === 'local'){
+    if(event.type === 'local' && flag){
+        flag = false;
         localVideosContainer.appendChild(video)
     }
 
@@ -30,6 +31,8 @@ connection.onstream = function(event){
 let roomid = document.getElementById('txt-roomid');
 
 roomid.value = connection.token();
+
+let flag = true;
 
 document.getElementById('btn-open-or-join-room').onclick = function(){
     this.disabled = true;
