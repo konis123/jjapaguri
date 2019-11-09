cbbbb8fca446847585d335858485cd58bc36735a
jjapa <konis123@naver.com>
2019-11-09 19:11:54 +0900

    test

--- public/js/multi.js
+++ public/js/multi.js
@@ -15,17 +15,18 @@ connection.sdpConstraints.mandatory = {
 
 var localVideosContainer = document.getElementById('local-videos-container')
 var remoteVideosContainer = document.getElementById('remote-videos-container')
+let videoTag;
 connection.onstream = function(event){
-    var video = event.mediaElement;
+    videoTag = event.mediaElement;
 
     if(event.type === 'local' && flag){
-        localVideosContainer.appendChild(video)
+        localVideosContainer.appendChild(videoTag)
     }
 
     if(event.type === 'remote' && !flag){
         flag = false;
-        remoteVideosContainer.appendChild(video)
-        console.log(video.id+'---------------')
+        remoteVideosContainer.appendChild(videoTag)
+        console.log(videoTag.id+'---------------')
     }
 
 };
@@ -61,3 +62,38 @@ document.getElementById('btn-open-or-join-room').onclick = async function(){
     connection.openOrJoin(roomid.value || 'predefiend-roomid')
 };
 
+
+//스타트, 엔드 버튼-----------
+var saveBtn = document.getElementById('start');
+saveBtn.addEventListener("click", async ()=>{
+    console.log('start click');
+    //mediaRecorder.save();
+
+    // if(remoteStream !== undefined){
+    //     var streamFromVideoTag = remoteVideo.captureStream(15); // 15 is frame-rates
+    //     recorder = RecordRTC(streamFromVideoTag, {type: 'video'});
+    //     recorder.startRecording();
+    // }else{
+    //     console.log('recorder undefined')
+    // }
+
+    recorder = new RecordRTC(videoTag, {
+        type: 'video',
+        mimeType: 'video/webm',
+    });
+    await recorder.startRecording();
+    recorder.camera = await remoteStream;
+    
+})
+
+var endBtn = document.getElementById('end');
+endBtn.addEventListener("click", async ()=>{
+    console.log('end click');
+    
+    recorder.stopRecording(function() {
+        let blob = recorder.getBlob();
+        invokeSaveAsDialog(blob,'test.mp4');
+    });
+
+
+});
