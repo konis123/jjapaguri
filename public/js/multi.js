ed891425c2c0ede0f0da64460128731fc27eadef
jjapa <konis123@naver.com>
2019-11-11 01:53:39 +0900

    Test

--- public/js/multi.js
+++ public/js/multi.js
@@ -3,12 +3,12 @@ let connection = new RTCMultiConnection();
 connection.socketURL = 'https://rtcmulticonnection.herokuapp.com:443/';//'localhost:3000'
 
 connection.session = {
-    audio: false,
+    // audio: false,
     video: true
 };
 
 connection.sdpConstraints.mandatory = {
-    offerToReceivaAudio: false,
+    // offerToReceivaAudio: false,
     offerToReceiveVideo: true
 };
 
@@ -38,19 +38,6 @@ roomid.value = connection.token();
 let flag = false;
 
 document.getElementById('btn-open-or-join-room').onclick = async function(){
-
-    // const options = {
-    //     uri:'http://ec2-15-164-224-142.ap-northeast-2.compute.amazonaws.com:8000/room/test', 
-    //     method: 'POST',
-    //     form: {
-    //       id: roomid.value,
-    //     }
-    // }
-
-    // const response = await request('http://ec2-15-164-224-142.ap-northeast-2.compute.amazonaws.com:8000/room/'+roomid.value, function (error, response, body) {
-    //     //callback
-    // });
-    // flag = response.result;
 
     $.get("https://jjapaguri.herokuapp.com/room/"+roomid.value, function(data){
         // flag = response.result;
